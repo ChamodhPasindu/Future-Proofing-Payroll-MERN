9a088a68e6f030a026bf89a31f7cb06b84cfdfd3
ChamodhPasindu <chamodhpasindu2000@gmail.com>
2024-06-21 12:35:28 +0530

    first implementation

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+// if(process.env.NODE_ENV === 'production'){
+// } else{
+//     module.exports = require('./keys_dev');
+// }
+
+module.exports = require('./keys_prod');
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Future-Proofing Payroll System
